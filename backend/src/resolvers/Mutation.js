4826961c91d857210a9bf53f8b808b3f2fd45102
Chase Harrison <ChaseH88@gmail.com>
2019-03-03 21:41:21 -0600

    Updated Mutations and Queries

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+const Mutations = {
+
+  async createItem(parent, args, ctx, info){
+    // Check if logged in
+
+    const item = await ctx.db.mutation.createItem({data: { ...args }}, info );
+      return item
+    }
+};
 
 module.exports = Mutations;
